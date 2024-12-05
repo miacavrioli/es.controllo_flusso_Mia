6e068bc7886f8ec05081112b91f92df120468576
miacavrioli <miacavrioli@gmail.com>
2024-12-05 20:26:16 +0100

    Esercizio controllo flusso

---
+++ es.controllo_flusso/main.js
@@ -0,0 +1,93 @@
+//Esercizi sul controllo del flusso di codice
+
+//Lancio di un dado
+
+// let max = 6;
+// let num = (Math.round(Math.random()*5)+1);
+
+// if (num>3){
+//     console.log(num, "è maggiore di 6")
+// }
+// else{
+//     console.log("Nulla da visualizzare")
+// }
+
+//Classificazione di un voto scolastico
+
+// let voto=(Math.random()*100)
+
+// if (voto>90){
+//     console.log("Ottimo!")
+// } else if (voto>89){
+//     console.log("Buono!")
+// } else if (voto>69){
+//     console.log("Sufficiente!")
+// } else if (60>voto>0){
+//     console.log("Insufficiente")
+// }
+
+//Identificazione della stagione
+
+/* let dicembre = 12 
+let gennaio = 1
+let febbraio = 2
+
+let marzo = 3 
+let aprile = 4
+let maggio = 5
+
+let giugno = 6 
+let luglio = 7
+let agosto = 8
+
+let settembre = 9 
+let ottobre = 10
+let novembre = 12
+
+let num=(Math.round(Math.random()*12)+1)
+
+switch (num){
+    case dicembre:
+        console.log("Inverno");
+        break;
+    case gennaio:
+        console.log("Inverno");
+        break;
+    case febbraio:
+        console.log("Inverno");
+        break;
+    case marzo:
+        console.log("Primavera");
+        break;
+    case aprile:
+        console.log("Primavera");
+        break;
+    case maggio:
+        console.log("Primavera");
+        break;
+    case giugno:
+        console.log("Estate");
+    break;
+    case luglio:
+        console.log("Estate");
+        break;
+    case agosto:
+        console.log("Estate");
+        break;
+    case settembre:
+        console.log("Autunno");
+        break;
+    case ottobre:
+        console.log("Autunno");
+        break;
+    case novembre:
+        console.log("Autunno");
+    } */
+
+//Verifica maggiore età
+
+let eta = (Math.round(Math.random()*110));
+
+(eta>18? (console.log("Stato: Maggiorenne")) : (console.log("Stato: Minorenne")))
+
+
